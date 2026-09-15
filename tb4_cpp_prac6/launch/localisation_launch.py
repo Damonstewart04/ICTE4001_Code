9658ded75ed7137f48bcb779b6bf50a1d447eac5
Sean McDougall <sean.mcdougall623@gmail.com>
2026-09-15 15:10:26 +0800

    fixed launcher

--- tb4_cpp_prac6/launch/localisation_launch.py
+++ tb4_cpp_prac6/launch/localisation_launch.py
@@ -9,19 +9,20 @@ from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterFile
 from nav2_common.launch import RewrittenYaml
 
+
 def generate_launch_description():
 
     # Get the launch directory
-    iar_amcl_dir = get_package_share_directory('tb4_cpp_prac6')
+    iar_amcl_dir = get_package_share_directory("tb4_cpp_prac6")
 
-    namespace = LaunchConfiguration('namespace')
-    map_yaml_file = LaunchConfiguration('map')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    autostart = LaunchConfiguration('autostart')
-    params_file = LaunchConfiguration('params_file')  
-    log_level = LaunchConfiguration('log_level') 
+    namespace = LaunchConfiguration("namespace")
+    map_yaml_file = LaunchConfiguration("map")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    autostart = LaunchConfiguration("autostart")
+    params_file = LaunchConfiguration("params_file")
+    log_level = LaunchConfiguration("log_level")
 
-    lifecycle_nodes = ['map_server', 'iar_amcl']
+    lifecycle_nodes = ["map_server", "iar_amcl"]
 
     # Map fully qualified names to relative ones so the node's namespace can be prepended.
     # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
@@ -29,85 +30,91 @@ def generate_launch_description():
     # https://github.com/ros/robot_state_publisher/pull/30
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
-    remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
-    
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+
     # Create our own temporary YAML files that include substitutions
-    param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'yaml_filename': map_yaml_file}
+    param_substitutions = {"use_sim_time": use_sim_time, "yaml_filename": map_yaml_file}
 
     configured_params = ParameterFile(
         RewrittenYaml(
             source_file=params_file,
             root_key=namespace,
             param_rewrites=param_substitutions,
-            convert_types=True),
-        allow_substs=True)
-    
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
     # Set env var to print messages to stdout immediately
     stdout_linebuf_envvar = SetEnvironmentVariable(
-        'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
+        "RCUTILS_LOGGING_BUFFERED_STREAM", "1"
+    )
 
     declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value='',
-        description='Top-level namespace')
-    
+        "namespace", default_value="", description="Top-level namespace"
+    )
+
     declare_map_yaml_cmd = DeclareLaunchArgument(
-        'map',
-        default_value=os.path.join(iar_amcl_dir, 'map', 'office_ICTE4001_sim.yaml'),
-        description='Full path to map yaml file to load')
-    
+        "map",
+        default_value=os.path.join(iar_amcl_dir, "map", "office_ICTE4001_sim.yaml"),
+        description="Full path to map yaml file to load",
+    )
+
     declare_use_sim_time_cmd = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='false',
-        description='Use simulation (Gazebo) clock if true')
-    
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
     declare_params_file_cmd = DeclareLaunchArgument(
-        'params_file',
-        default_value=os.path.join(iar_amcl_dir, 'config', 'localisation.yaml'),
-        description='Full path to the ROS2 parameters file to use for localisation')
-    
+        "params_file",
+        default_value=os.path.join(iar_amcl_dir, "config", "localisation.yaml"),
+        description="Full path to the ROS2 parameters file to use for localisation",
+    )
+
     declare_autostart_cmd = DeclareLaunchArgument(
-        'autostart', default_value='true',
-        description='Automatically startup the nav2 stack')
-    
+        "autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    )
+
     declare_log_level_cmd = DeclareLaunchArgument(
-        'log_level', default_value='info',
-        description='log level')
-    
+        "log_level", default_value="info", description="log level"
+    )
+
     map_server_node = Node(
-        package='nav2_map_server',
-        executable='map_server',
-        name='map_server',
-        output='screen',
+        package="nav2_map_server",
+        executable="map_server",
+        name="map_server",
+        output="screen",
         parameters=[configured_params],
-        arguments=['--ros-args', '--log-level', log_level],
-        remappings=remappings
+        arguments=["--ros-args", "--log-level", log_level],
+        remappings=remappings,
     )
-    
+
     iar_amcl_node = Node(
-        package='tb4_cpp_prac5',
-        executable='iar_amcl',
-        name='iar_amcl',
-        output='screen',
+        package="tb4_cpp_prac6",
+        executable="iar_amcl",
+        name="iar_amcl",
+        output="screen",
         parameters=[configured_params],
-        arguments=['--ros-args', '--log-level', log_level],
-        remappings=remappings
+        arguments=["--ros-args", "--log-level", log_level],
+        remappings=remappings,
     )
-    
+
     lifecycle_manager_node = Node(
-        package='nav2_lifecycle_manager',
-        executable='lifecycle_manager',
-        name='lifecycle_manager_localization',
-        output='screen',
-        arguments=['--ros-args', '--log-level', log_level],
-        parameters=[{'use_sim_time': use_sim_time},
-                    {'autostart': autostart},
-                    {'node_names': lifecycle_nodes}]
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_localization",
+        output="screen",
+        arguments=["--ros-args", "--log-level", log_level],
+        parameters=[
+            {"use_sim_time": use_sim_time},
+            {"autostart": autostart},
+            {"node_names": lifecycle_nodes},
+        ],
     )
-    
+
     # Create the launch description and populate
     ld = LaunchDescription()
     ld.add_action(stdout_linebuf_envvar)
